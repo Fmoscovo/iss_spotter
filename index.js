441abe6482b3cb139a4d9df4b9c70dbe15fa5ce9
Filipe Moscovo <your@email.com>
2023-07-06 22:41:15 -0600

    fix

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchCoordsByIP } = require("./iss");
 
-fetchMyIP((error, ip) => {
+fetchCoordsByIP("162.245.144.188", (error, coordinates) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  console.log("It worked! Returned IP:", ip);
-
-  fetchCoordsByIP(ip, (error, data) => {
-    if (error) {
-      console.log("It didn't work!", error);
-      return;
-    }
-
-    console.log("It worked! Returned Coords:", data);
-  });
+  console.log("It worked! Returned coordinates:", coordinates);
 });
 
-// // The code below is temporary and can be commented out.
-// const { fetchCoordsByIP } = require('./iss');
+//////////////////my previous code///////////////////
+// const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
-// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+// fetchMyIP((error, ip) => {
 //   if (error) {
-//     console.log("It didn't work!" , error);
+//     console.log("It didn't work!", error);
 //     return;
 //   }
 
-//   console.log('It worked! Returned coordinates:' , coordinates);
+//   console.log("It worked! Returned IP:", ip);
+
+//   fetchCoordsByIP(ip, (error, data) => {
+//     if (error) {
+//       console.log("It didn't work!", error);
+//       return;
+//     }
+
+//     console.log("It worked! Returned Coords:", data);
+//   });
 // });
+
+// // The code below is temporary and can be commented out.
